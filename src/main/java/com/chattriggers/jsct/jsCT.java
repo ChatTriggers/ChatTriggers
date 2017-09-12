5c5daa711c40d79dcb818c5e61fb3df67891978e
FalseHonesty <skipboman0@gmail.com>
2017-09-12 19:35:06 -0400

    Created mod base

---
+++ src/main/java/com/chattriggers/jsct/jsCT.java
@@ -0,0 +1,18 @@
+package com.chattriggers.jsct;
+
+import net.minecraftforge.fml.common.Mod;
+
+/**
+ * Copyright (c) FalseHonesty 2017
+ */
+
+@Mod(modid = Reference.MODID, name = Reference.MODNAME, version = Reference.MODVERSION)
+public class jsCT {
+
+}
+
+class Reference {
+    public static final String MODID = "jsct";
+    public static final String MODNAME = "jsChatTriggers";
+    public static final String MODVERSION = "0.1-SNAPSHOT";
+}
