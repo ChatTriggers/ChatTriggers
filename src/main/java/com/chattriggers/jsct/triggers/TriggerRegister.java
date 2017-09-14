29f4a777b9b5b5f7aa96e61d35c20c7f35d407ca
FalseHonesty <skipboman0@gmail.com>
2017-09-13 20:36:02 -0400

    Added more comments

--- src/main/java/com/chattriggers/jsct/triggers/TriggerRegister.java
+++ src/main/java/com/chattriggers/jsct/triggers/TriggerRegister.java
@@ -4,6 +4,12 @@ import java.util.ArrayList;
 
 public class TriggerRegister {
 
+    /**
+     * Register a new method that receives chat events based
+     * on certain message criteria
+     * @param methodName the name of the method to callback when the event is called
+     * @param chatCriteria the criteria for which the event should called
+     */
     public static void registerChat(String methodName, String chatCriteria) {
         ChatTrigger trigger = new ChatTrigger(methodName, chatCriteria);
         TriggerTypes.CHAT.addTrigger(trigger);
