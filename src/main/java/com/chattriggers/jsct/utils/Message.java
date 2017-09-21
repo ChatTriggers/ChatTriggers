31b21de91b51187396400869ff2e166132e0026e
FalseHonesty <skipboman0@gmail.com>
2017-09-20 20:13:15 -0400

    added more helper methods to Message class

--- src/main/java/com/chattriggers/jsct/utils/Message.java
+++ src/main/java/com/chattriggers/jsct/utils/Message.java
@@ -2,17 +2,43 @@ package com.chattriggers.jsct.utils;
 
 import com.chattriggers.jsct.libs.ChatLib;
 import lombok.Getter;
-import net.minecraft.nbt.NBTTagList;
-import net.minecraft.nbt.NBTTagString;
 import net.minecraft.util.*;
+
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class Message {
     @Getter
     private IChatComponent chatMessage;
+    private ArrayList<Object> messageParts;
 
     public Message(Object... messages) {
         chatMessage = new ChatComponentText("");
+        messageParts = new ArrayList<>();
 
+        Collections.addAll(messageParts, messages);
+        parseMessages(messages);
+    }
+
+    /**
+     * Sets a part of the message (defined by the splits made in the constructor)
+     * @param part the index of the part to change
+     * @param message the new message to replace with
+     */
+    public void setMessagePart(int part, Object message) {
+        messageParts.set(part, message);
+        parseMessages(messageParts);
+    }
+
+    /**
+     * Get an exact copy of the message
+     * @return the copy of the message
+     */
+    public Message copy() {
+        return new Message(messageParts);
+    }
+
+    private void parseMessages(Object... messages) {
         for (Object message : messages) {
             if (message instanceof String) {
                 ChatComponentText cct = new ChatComponentText(ChatLib.addColor((String) message));
