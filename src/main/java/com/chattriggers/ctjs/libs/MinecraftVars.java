f0b401f1ff6133db567467cab5ceff931855fa0b
Andrew Koerber <kerbybit@gmail.com>
2017-10-11 21:00:25 -0500

    removed prev camera and fixed pitch and yaw

--- src/main/java/com/chattriggers/ctjs/libs/MinecraftVars.java
+++ src/main/java/com/chattriggers/ctjs/libs/MinecraftVars.java
@@ -309,16 +309,7 @@ public class MinecraftVars {
      * @return The player's camera pitch.
      */
     public static Float getPlayerPitch() {
-        return mc.thePlayer == null ? null : MathHelper.wrapAngleTo180_float(mc.thePlayer.cameraPitch);
-    }
-
-    /**
-     * Gets the player's previous camera pitch.
-     * In an import, accessible via the {@code cameraPrevPitch} variable.
-     * @return The player's camera pitch.
-     */
-    public static Float getPrevPlayerPitch() {
-        return mc.thePlayer == null ? null : MathHelper.wrapAngleTo180_float(mc.thePlayer.prevRotationPitch);
+        return mc.thePlayer == null ? null : MathHelper.wrapAngleTo180_float(mc.thePlayer.rotationPitch);
     }
 
     /**
@@ -328,15 +319,6 @@ public class MinecraftVars {
      */
     public static Float getPlayerYaw() {
         return mc.thePlayer == null ? null : MathHelper.wrapAngleTo180_float(mc.thePlayer.rotationYaw);
-    }
-
-    /**
-     * Gets the player's previous camera yaw.
-     * In an import, accessible via the {@code cameraPrevYaw} variable.
-     * @return The player's camera pitch.
-     */
-    public static Float getPrevPlayerYaw() {
-        return mc.thePlayer == null ? null : MathHelper.wrapAngleTo180_float(mc.thePlayer.prevRotationYaw);
     }
 
     /**
