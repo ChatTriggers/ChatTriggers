9db07515a93df837c8789be0cb7fc5a961a45ec4
Andrew Koerber <kerbybit@gmail.com>
2017-10-17 08:25:38 -0500

    Added more trigger documentation

--- src/main/java/com/chattriggers/ctjs/triggers/OnTrigger.java
+++ src/main/java/com/chattriggers/ctjs/triggers/OnTrigger.java
@@ -18,21 +18,39 @@ public abstract class OnTrigger {
         this.register();
     }
 
+    /**
+     * Sets a triggers priority using {@link Priority}.
+     * Highest runs first.
+     * @param priority the priority of the trigger
+     * @return the trigger for method chaining
+     */
     public OnTrigger setPriority(Priority priority) {
         this.priority = priority;
         return this;
     }
 
+    /**
+     * Registers a trigger based on its type.
+     * This is done automatically with TriggerRegister.
+     * @return the trigger for method chaining
+     */
     public OnTrigger register() {
         this.type.addTrigger(this);
         return this;
     }
 
+    /**
+     * Unregisters a trigger.
+     * @return the trigger for method chaining
+     */
     public OnTrigger unregister() {
         this.type.removeTrigger(this);
         return this;
     }
 
+    /**
+     * @return boolean of if trigger is registered
+     */
     public boolean isRegistered() {
         return this.type.containsTrigger(this);
     }
