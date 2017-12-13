ce1b1b5a414af6825b155da6e4dd315ff0f120da
FalseHonesty <skipboman0@gmail.com>
2017-12-13 16:30:17 -0500

    Changed version number --release snapshot

--- src/main/java/com/chattriggers/ctjs/Reference.java
+++ src/main/java/com/chattriggers/ctjs/Reference.java
@@ -3,6 +3,6 @@ package com.chattriggers.ctjs;
 public class Reference {
     public static final String MODID = "ct.js";
     public static final String MODNAME = "ChatTriggers.js";
-    public static final String MODVERSION = "0.2-SNAPSHOT";
+    public static final String MODVERSION = "0.3.1-SNAPSHOT";
     public static final String MAPPINGURL = "http://export.mcpbot.bspk.rs/mcp_stable_nodoc/22-1.8.9/mcp_stable_nodoc-22-1.8.9.zip";
 }
