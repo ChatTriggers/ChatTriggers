4273273481ee3f77a1eb5e082ab27cfc11c2a0e7
Andrew Koerber <kerbybit@gmail.com>
2017-12-20 15:36:48 -0600

    fixed priority --release snapshot

--- src/main/java/com/chattriggers/ctjs/Reference.java
+++ src/main/java/com/chattriggers/ctjs/Reference.java
@@ -5,7 +5,7 @@ import net.minecraft.launchwrapper.Launch;
 public class Reference {
     public static final String MODID = "ct.js";
     public static final String MODNAME = "ChatTriggers.js";
-    public static final String MODVERSION = "0.3.3-SNAPSHOT";
+    public static final String MODVERSION = "0.3.4-SNAPSHOT";
     public static final String MAPPINGURL = "http://export.mcpbot.bspk.rs/mcp_stable_nodoc/22-1.8.9/mcp_stable_nodoc-22-1.8.9.zip";
     public static final String SENTRYDSN = "https://a69c5c01577c457b88434de9b995ceec:317ddf76172b4020b80f79befe536f98@sentry.io/259416"
                     + "?release=" + MODVERSION
