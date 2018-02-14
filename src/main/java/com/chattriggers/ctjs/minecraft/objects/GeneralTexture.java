90d1ceb86f2591f4177d9cb3dcb84a2498f13e81
Hunter <thefalsehonesty@gmail.com>
2018-02-14 18:32:26 -0500

    Add particle wrapper

---
+++ src/main/java/com/chattriggers/ctjs/minecraft/objects/GeneralTexture.java
@@ -0,0 +1,11 @@
+package com.chattriggers.ctjs.minecraft.objects;
+
+import net.minecraft.client.renderer.texture.TextureAtlasSprite;
+import net.minecraft.util.ResourceLocation;
+
+public class GeneralTexture extends TextureAtlasSprite {
+    public GeneralTexture(String spriteName) {
+//        ResourceLocation rl = new ResourceLocation("ctjs.images", spriteName);
+        super(spriteName);
+    }
+}
--- src/main/java/com/chattriggers/ctjs/minecraft/wrappers/World.java
+++ src/main/java/com/chattriggers/ctjs/minecraft/wrappers/World.java
@@ -1,6 +1,16 @@
 package com.chattriggers.ctjs.minecraft.wrappers;
 
+import com.chattriggers.ctjs.minecraft.libs.WorldLib;
+import com.chattriggers.ctjs.minecraft.objects.GeneralTexture;
+import com.chattriggers.ctjs.utils.console.Console;
+import lombok.Getter;
 import net.minecraft.client.multiplayer.WorldClient;
+import net.minecraft.client.particle.EntityFX;
+import net.minecraft.client.renderer.texture.TextureAtlasSprite;
+import net.minecraft.util.EnumParticleTypes;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 public class World {
     /**
@@ -138,6 +148,90 @@ public class World {
          */
         public static int getZ() {
             return getWorld().getSpawnPoint().getZ();
+        }
+    }
+
+    public static class particle {
+        @Getter
+        private EntityFX underlyingEntity;
+
+        public particle(EntityFX entityFX) {
+            this.underlyingEntity = entityFX;
+        }
+
+        public void scale(float scale) {
+            this.underlyingEntity.multipleParticleScaleBy(scale);
+        }
+
+        public void multiplyVelocity(float multiplier) {
+            this.underlyingEntity.multiplyVelocity(multiplier);
+        }
+
+        public void setColor(float r, float g, float b) {
+            this.underlyingEntity.setRBGColorF(r, g, b);
+        }
+
+        public void setColor(float r, float g, float b, float a) {
+            setColor(r, g, b);
+
+            setAlpha(a);
+        }
+
+        public void setColor(int color) {
+            float red = (float)(color >> 16 & 255) / 255.0F;
+            float blue = (float)(color >> 8 & 255) / 255.0F;
+            float green = (float)(color & 255) / 255.0F;
+            float alpha = (float)(color >> 24 & 255) / 255.0F;
+
+            setColor(red, green, blue, alpha);
+        }
+
+        public void setAlpha(float a) {
+            this.underlyingEntity.setAlphaF(a);
+        }
+
+        /*public void setTexture(String textureName) {
+            this.underlyingEntity.setParticleIcon(new GeneralTexture(textureName));
+        }*/
+
+        /**
+         * Gets an array of all the different particle names you can pass to {@link #spawnParticle(String, double, double, double, double, double, double)}
+         * @return the array of name strings
+         */
+        public static String[] getParticleNames() { return EnumParticleTypes.getParticleNames(); }
+
+        public static particle spawnParticle(String particle, double x, double y, double z, double dx, double dy, double dz) {
+            EnumParticleTypes particleType = EnumParticleTypes.valueOf(particle);
+
+            try {
+                Method method = Client.getMinecraft().renderGlobal.getClass()
+                        .getDeclaredMethod(
+                                "spawnEntityFX",
+                                int.class,
+                                boolean.class,
+                                double.class,
+                                double.class,
+                                double.class,
+                                double.class,
+                                double.class,
+                                double.class
+                        );
+
+                method.setAccessible(true);
+
+
+                EntityFX fx = (EntityFX) method.invoke(Client.getMinecraft().renderGlobal,
+                        particleType.getParticleID(),
+                        particleType.getShouldIgnoreRange(),
+                        x, y, z, dx, dy, dz
+                );
+
+                return new particle(fx);
+            } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException e) {
+                Console.getConsole().printStackTrace(e);
+            }
+
+            return null;
         }
     }
 }
