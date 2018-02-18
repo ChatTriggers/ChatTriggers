e6bcca6d5adaf873534534e884b813657546b539
Hunter <thefalsehonesty@gmail.com>
2018-02-18 03:13:18 -0500

    Started adding entity wrapper

---
+++ src/main/java/com/chattriggers/ctjs/minecraft/wrappers/objects/Entity.java
@@ -0,0 +1,140 @@
+package com.chattriggers.ctjs.minecraft.wrappers.objects;
+
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.util.MathHelper;
+
+public class Entity {
+    private net.minecraft.entity.Entity entity;
+
+    public Entity(net.minecraft.entity.Entity entity) {
+        this.entity = entity;
+    }
+
+    /**
+     * @return the entity's x coordinate
+     */
+    public double getX() {
+        return this.entity.posX;
+    }
+
+    /**
+     * @return the entity's y coordinate
+     */
+    public double getY() {
+        return this.entity.posY;
+    }
+
+    /**
+     * @return the entity's z coordinate
+     */
+    public double getZ() {
+        return this.entity.posZ;
+    }
+
+    /**
+     * Gets the pitch, the horizontal direction the entity is facing towards.
+     * This has a range of -180 to 180.
+     *
+     * @return the entity's pitch
+     */
+    public double getPitch() {
+        return MathHelper.wrapAngleTo180_float(this.entity.rotationPitch);
+    }
+
+    /**
+     * Gets the yaw, the vertical direction the entity is facing towards.
+     * This has a range of -180 to 180.
+     *
+     * @return the entity's yaw
+     */
+    public double getYaw() {
+        return MathHelper.wrapAngleTo180_float(this.entity.rotationYaw);
+    }
+
+    /**
+     * Gets the entity's x motion.
+     * This is the amount the entity will move in the x direction next tick.
+     *
+     * @return the player's x motion
+     */
+    public double getMotionX() {
+        return this.entity.motionX;
+    }
+
+    /**
+     * Gets the entity's y motion.
+     * This is the amount the entity will move in the y direction next tick.
+     *
+     * @return the player's y motion
+     */
+    public double getMotionY() {
+        return this.entity.motionY;
+    }
+
+    /**
+     * Gets the entity's z motion.
+     * This is the amount the entity will move in the z direction next tick.
+     *
+     * @return the player's z motion
+     */
+    public double getMotionZ() {
+        return this.entity.motionZ;
+    }
+
+    /**
+     * Gets the entity's health, 0 if not a living entity
+     *
+     * @return the entity's health
+     */
+    public float getHP() {
+        return this.entity instanceof EntityLivingBase
+                ? ((EntityLivingBase) this.entity).getHealth()
+                : 0;
+    }
+
+    /**
+     * Gets the entity that this entity is riding.
+     *
+     * @return the entity being ridden, or null if there isn't one.
+     */
+    public Entity getRiding() {
+        return this.entity.ridingEntity == null
+                ? null
+                : new Entity(this.entity.ridingEntity);
+    }
+
+    /**
+     * Gets the entity that is riding this entity.
+     *
+     * @return the riding entity, or null if there isn't one.
+     */
+    public Entity getRider() {
+        return this.entity.riddenByEntity == null
+                ? null
+                : new Entity(this.entity.riddenByEntity);
+    }
+
+    public boolean isDead() {
+        return this.entity.isDead;
+    }
+
+    public float getWidth() {
+        return this.entity.width;
+    }
+
+    public float getHeight() {
+        return this.entity.height;
+    }
+
+    public float getEyeHeight() {
+        return this.entity.getEyeHeight();
+    }
+
+    public String getName() {
+        return this.entity.getName();
+    }
+
+    public String getClassName() {
+        return this.entity.getClass().getSimpleName();
+    }
+}
