cd8613e59fc8958c64b8e3f8778ed3be0e8d4cfe
FalseHonesty <skipboman0@gmail.com>
2018-03-07 18:29:48 -0500

    Expanded player mp

--- src/main/java/com/chattriggers/ctjs/minecraft/wrappers/objects/PlayerMP.java
+++ src/main/java/com/chattriggers/ctjs/minecraft/wrappers/objects/PlayerMP.java
@@ -1,8 +1,12 @@
 package com.chattriggers.ctjs.minecraft.wrappers.objects;
 
+import com.chattriggers.ctjs.minecraft.wrappers.Client;
+import lombok.Getter;
 import net.minecraft.client.entity.EntityOtherPlayerMP;
+import net.minecraft.client.network.NetworkPlayerInfo;
 
 public class PlayerMP extends Entity {
+    @Getter
     private EntityOtherPlayerMP player;
 
     public PlayerMP(EntityOtherPlayerMP player) {
@@ -18,5 +22,29 @@ public class PlayerMP extends Entity {
 
     public boolean isSpectator() {
         return this.player.isSpectator();
+    }
+
+    public int getPing() {
+        return getPlayerInfo().getResponseTime();
+    }
+
+    /**
+     * Gets the item currently in the player's specified inventory slot.
+     * 0 for main hand, 1-4 for armor.
+     *
+     * @param slot the slot to access
+     * @return the item in said slot
+     */
+    public Item getItemInSlot(int slot) {
+        return new Item(player.getEquipmentInSlot(slot));
+    }
+
+    public NetworkPlayerInfo getPlayerInfo() {
+        return Client.getMinecraft().getNetHandler().getPlayerInfo(this.player.getUniqueID());
+    }
+
+    @Override
+    public String toString() {
+        return "PlayerMP{name:" + getName() + ",ping:" + getPing() + ",entity:" + super.toString() + "}";
     }
 }
