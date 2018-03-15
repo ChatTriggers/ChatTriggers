47a54445a22337a2978af735e1747a1b9ba2b7d7
Andrew Koerber <kerbybit@gmail.com>
2018-03-14 21:46:39 -0500

    Added Tessellator colorize, rotate, translate, scale
    order needs to be fixed because of the player position translate in "begin"

--- src/main/java/com/chattriggers/ctjs/minecraft/libs/Tessellator.java
+++ src/main/java/com/chattriggers/ctjs/minecraft/libs/Tessellator.java
@@ -2,6 +2,7 @@ package com.chattriggers.ctjs.minecraft.libs;
 
 import com.chattriggers.ctjs.minecraft.wrappers.Client;
 import net.minecraft.client.renderer.GlStateManager;
+import net.minecraft.client.renderer.RenderHelper;
 import net.minecraft.client.renderer.WorldRenderer;
 import net.minecraft.client.renderer.entity.RenderManager;
 import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
@@ -12,11 +13,15 @@ public class Tessellator {
     private net.minecraft.client.renderer.Tessellator tessellator;
     private WorldRenderer worldRenderer;
     private boolean firstVertex;
+    private boolean began;
+    private boolean colorized;
 
     public Tessellator() {
         this.tessellator = net.minecraft.client.renderer.Tessellator.getInstance();
         this.worldRenderer = this.tessellator.getWorldRenderer();
         this.firstVertex = true;
+        this.began = false;
+        this.colorized = false;
     }
 
     public Tessellator bindTexture(String texture, String domain) {
@@ -35,16 +40,13 @@ public class Tessellator {
 
         GlStateManager.enableBlend();
         GlStateManager.tryBlendFuncSeparate(770, 771, 1, 0);
-        //GlStateManager.disableTexture2D();
-
-        // TODO: NEEDS OWN FUNC
-        GL11.glColor4f(1F, 1F, 1F, 1F);
 
         RenderManager renderManager = Client.getMinecraft().getRenderManager();
         GlStateManager.translate(-renderManager.viewerPosX, -renderManager.viewerPosY, -renderManager.viewerPosZ);
 
         this.worldRenderer.begin(drawMode, textured ? DefaultVertexFormats.POSITION_TEX : DefaultVertexFormats.POSITION);
         this.firstVertex = true;
+        this.began = true;
 
         return this;
     }
@@ -53,7 +55,38 @@ public class Tessellator {
         return begin(GL11.GL_QUADS, true);
     }
 
+    public Tessellator colorize(float red, float green, float blue, float alpha) {
+        GlStateManager.color(red, green, blue, alpha);
+        this.colorized = true;
+
+        return this;
+    }
+
+    public Tessellator rotate(float angle, float x, float y, float z) {
+        GlStateManager.rotate(angle, x, y, z);
+
+        return this;
+    }
+
+    public Tessellator translate(float x, float y, float z) {
+        GlStateManager.translate(x, y, z);
+
+        return this;
+    }
+
+    public Tessellator scale(float x, float y, float z) {
+        GlStateManager.scale(x, y, z);
+
+        return this;
+    }
+
+    public Tessellator scale(float scale) {
+        return scale(scale, scale, scale);
+    }
+
     public Tessellator pos(float x, float y, float z) {
+        if (!this.began)
+            this.begin();
         if (!this.firstVertex)
             this.worldRenderer.endVertex();
         this.worldRenderer.pos(x, y, z);
@@ -70,10 +103,15 @@ public class Tessellator {
 
     public void draw() {
         this.worldRenderer.endVertex();
+
+        if (!this.colorized)
+            colorize(1F, 1F, 1F, 1F);
+
         this.tessellator.draw();
+        this.began = false;
+        this.colorized = false;
 
         GlStateManager.disableBlend();
-        GlStateManager.enableTexture2D();
 
         GlStateManager.popMatrix();
     }
