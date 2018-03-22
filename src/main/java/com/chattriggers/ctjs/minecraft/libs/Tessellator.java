0ae9a3f5284f592fec9e0be99a44e19b03e63795
Andrew Koerber <kerbybit@gmail.com>
2018-03-22 13:20:31 -0500

    Added docs to Tessellator
    removed auto deploy in travis for now

--- src/main/java/com/chattriggers/ctjs/minecraft/libs/Tessellator.java
+++ src/main/java/com/chattriggers/ctjs/minecraft/libs/Tessellator.java
@@ -24,6 +24,13 @@ public class Tessellator {
         this.colorized = false;
     }
 
+    /**
+     * Binds a texture to the client for the Tessellator to use.
+     *
+     * @param texture the texture to bind
+     * @param domain the domain of the texture
+     * @return the Tessellator to allow for method chaining
+     */
     public Tessellator bindTexture(String texture, String domain) {
         ResourceLocation rl = new ResourceLocation(domain, texture);
         Client.getMinecraft().getTextureManager().bindTexture(rl);
@@ -31,10 +38,25 @@ public class Tessellator {
         return this;
     }
 
+    /**
+     * Binds a texture to the client for the Tessellator to use
+     * with the default domain of "ctjs.images".
+     *
+     * @param texture the texture to bind
+     * @return the Tessellator to allow for method chaining
+     */
     public Tessellator bindTexture(String texture) {
         return bindTexture(texture, "ctjs.images");
     }
 
+    /**
+     * Begin drawing with the Tessellator.
+     *
+     * @see com.chattriggers.ctjs.minecraft.libs.renderer.Shape#setDrawMode(int)
+     * @param drawMode the GL draw mode
+     * @param textured if the Tessellator is textured
+     * @return the Tessellator to allow for method chaining
+     */
     public Tessellator begin(int drawMode, boolean textured) {
         GlStateManager.pushMatrix();
 
@@ -51,10 +73,25 @@ public class Tessellator {
         return this;
     }
 
+    /**
+     * Begin drawing with the Tessellator
+     * with default draw mode of quads and textured.
+     *
+     * @return the Tessellator to allow for method chaining
+     */
     public Tessellator begin() {
         return begin(GL11.GL_QUADS, true);
     }
 
+    /**
+     * Colorize the Tessellator.
+     *
+     * @param red the red value between 0 and 1
+     * @param green the green value between 0 and 1
+     * @param blue the blue value between 0 and 1
+     * @param alpha the alpha value between 0 and 1
+     * @return the Tessellator to allow for method chaining
+     */
     public Tessellator colorize(float red, float green, float blue, float alpha) {
         GlStateManager.color(red, green, blue, alpha);
         this.colorized = true;
@@ -62,28 +99,71 @@ public class Tessellator {
         return this;
     }
 
+    /**
+     * Rotates the Tessellator in 3d space.
+     * Similar to {@link com.chattriggers.ctjs.minecraft.libs.renderer.Renderer#rotate(float)}
+     *
+     * @param angle the angle to rotate
+     * @param x if the rotation is around the x axis
+     * @param y if the rotation is around the y axis
+     * @param z if the rotation is around the z axis
+     * @return the Tessellator to allow for method chaining
+     */
     public Tessellator rotate(float angle, float x, float y, float z) {
         GlStateManager.rotate(angle, x, y, z);
 
         return this;
     }
 
+    /**
+     * Translates the Tessellator in 3d space.
+     * Similar to {@link com.chattriggers.ctjs.minecraft.libs.renderer.Renderer#translate(float, float)}
+     *
+     * @param x the x position
+     * @param y the y position
+     * @param z the z position
+     * @return the Tessellator to allow for method chaining
+     */
     public Tessellator translate(float x, float y, float z) {
         GlStateManager.translate(x, y, z);
 
         return this;
     }
 
+    /**
+     * Scales the Tessellator in 3d space.
+     * Similar to {@link com.chattriggers.ctjs.minecraft.libs.renderer.Renderer#scale(float, float)}
+     *
+     * @param x scale in the x direction
+     * @param y scale in the y direction
+     * @param z scale in the z direction
+     * @return the Tessellator to allow for method chaining
+     */
     public Tessellator scale(float x, float y, float z) {
         GlStateManager.scale(x, y, z);
 
         return this;
     }
 
+    /**
+     * Scales the Tessellator in equally 3d space.
+     * Similar to {@link com.chattriggers.ctjs.minecraft.libs.renderer.Renderer#scale(float)}
+     *
+     * @param scale the scale
+     * @return the Tessellator to allow for method chaining
+     */
     public Tessellator scale(float scale) {
         return scale(scale, scale, scale);
     }
 
+    /**
+     * Sets a new vertex in the Tessellator.
+     *
+     * @param x the x position
+     * @param y the y position
+     * @param z the z position
+     * @return the Tessellator to allow for method chaining
+     */
     public Tessellator pos(float x, float y, float z) {
         if (!this.began)
             this.begin();
@@ -95,13 +175,26 @@ public class Tessellator {
         return this;
     }
 
+    /**
+     * Sets the texture location on the last defined vertex.
+     * Use directly after using {@link Tessellator#pos(float, float, float)}
+     *
+     * @param u the u position in the texture
+     * @param v the v position in the texture
+     * @return the Tessellator to allow for method chaining
+     */
     public Tessellator tex(float u, float v) {
         this.worldRenderer.tex(u, v);
 
         return this;
     }
 
+    /**
+     * Finalizes and draws the Tessellator.
+     */
     public void draw() {
+        if (!began) return;
+
         this.worldRenderer.endVertex();
 
         if (!this.colorized)
