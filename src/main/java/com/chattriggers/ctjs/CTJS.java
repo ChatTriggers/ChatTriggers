84435ca9e996c1ff160e2da0e6aaf032cea5b478
Andrew Koerber <kerbybit@gmail.com>
2018-03-25 14:41:25 -0500

    removed unused imports

--- src/main/java/com/chattriggers/ctjs/CTJS.java
+++ src/main/java/com/chattriggers/ctjs/CTJS.java
@@ -13,8 +13,6 @@ import com.chattriggers.ctjs.minecraft.listeners.ChatListener;
 import com.chattriggers.ctjs.minecraft.listeners.ClientListener;
 import com.chattriggers.ctjs.minecraft.listeners.WorldListener;
 import com.chattriggers.ctjs.minecraft.objects.CPS;
-import com.chattriggers.ctjs.minecraft.objects.display.DisplayHandler;
-import com.chattriggers.ctjs.minecraft.objects.gui.GuiHandler;
 import com.chattriggers.ctjs.minecraft.wrappers.Player;
 import com.chattriggers.ctjs.triggers.TriggerType;
 import com.chattriggers.ctjs.utils.ImagesPack;
