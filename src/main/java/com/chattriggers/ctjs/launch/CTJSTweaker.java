b3d282b4cb589f908c02795af8b9ea507494a2c3
Andrew Koerber <kerbybit@gmail.com>
2018-04-17 20:30:49 -0500

    fixed mixins (thanks to ReplayMod)

--- src/main/java/com/chattriggers/ctjs/launch/CTJSTweaker.java
+++ src/main/java/com/chattriggers/ctjs/launch/CTJSTweaker.java
@@ -2,11 +2,15 @@ package com.chattriggers.ctjs.launch;
 
 import net.minecraft.launchwrapper.ITweaker;
 import net.minecraft.launchwrapper.LaunchClassLoader;
+import net.minecraftforge.fml.relauncher.CoreModManager;
 import org.spongepowered.asm.launch.MixinBootstrap;
 import org.spongepowered.asm.mixin.MixinEnvironment;
 import org.spongepowered.asm.mixin.Mixins;
 
 import java.io.File;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.security.CodeSource;
 import java.util.List;
 
 public class CTJSTweaker implements ITweaker {
@@ -22,6 +26,23 @@ public class CTJSTweaker implements ITweaker {
         Mixins.addConfiguration("mixins.ctjs.json");
         MixinEnvironment.getDefaultEnvironment().setObfuscationContext("searge");
         MixinEnvironment.getDefaultEnvironment().setSide(MixinEnvironment.Side.CLIENT);
+
+        // fixing mixins. code from https://github.com/ReplayMod/ReplayMod/blob/0a26f59c9c5ac6ec72b8bdb758ef126f2d10e28d/src/main/java/com/replaymod/core/LoadingPlugin.java#L26
+        CodeSource codeSource = getClass().getProtectionDomain().getCodeSource();
+        if (codeSource != null) {
+            URL location = codeSource.getLocation();
+            try {
+                File file = new File(location.toURI());
+                if (file.isFile()) {
+                    CoreModManager.getIgnoredMods().remove(file.getName());
+                }
+            } catch (URISyntaxException e) {
+                e.printStackTrace();
+            }
+        } else {
+            System.out.println("No CodeSource, if this is not a development environment we might run into problems!");
+            System.out.println(getClass().getProtectionDomain());
+        }
     }
 
     @Override
