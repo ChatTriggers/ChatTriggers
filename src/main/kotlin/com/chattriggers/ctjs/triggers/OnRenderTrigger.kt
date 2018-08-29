d64c207f21527240c6f0ac918196780cb32b9e6b
Matthew Olsson <matthewcolsson@gmail.com>
2018-08-28 21:15:37 -0500

    Converted all OnTrigger classes (#143)

---
+++ src/main/kotlin/com/chattriggers/ctjs/triggers/OnRenderTrigger.kt
@@ -0,0 +1,28 @@
+package com.chattriggers.ctjs.triggers
+
+import net.minecraft.client.renderer.GlStateManager
+import net.minecraftforge.client.event.RenderGameOverlayEvent
+
+class OnRenderTrigger(method: Any, triggerType: TriggerType) : OnTrigger(method, triggerType) {
+    private var triggerIfCanceled: Boolean = true
+
+    /**
+     * Sets if the render trigger should run if the event has already been canceled.
+     * True by default.
+     * @param bool Boolean to set
+     * @return the trigger object for method chaining
+     */
+    fun triggerIfCanceled(bool: Boolean) = apply { this.triggerIfCanceled = bool }
+
+    override fun trigger(vararg args: Any) {
+        if (args[0] !is RenderGameOverlayEvent)
+            throw IllegalArgumentException("Argument 0 must be a RenderGameOverlayEvent")
+
+        val event = args[0] as RenderGameOverlayEvent
+        if (!triggerIfCanceled && event.isCanceled) return
+
+        GlStateManager.pushMatrix()
+        callMethod(*args)
+        GlStateManager.popMatrix()
+    }
+}
