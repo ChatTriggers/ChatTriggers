ca19a37c6c25324903f18a97edabee89f841ecdc
Andrew Koerber <kerbybit@gmail.com>
2018-09-08 10:25:47 -0500

    Fixed tweaker and missing getter in Text

--- src/main/kotlin/com/chattriggers/ctjs/launch/CTJSTweaker.kt
+++ src/main/kotlin/com/chattriggers/ctjs/launch/CTJSTweaker.kt
@@ -2,11 +2,13 @@ package com.chattriggers.ctjs.launch
 
 import net.minecraft.launchwrapper.ITweaker
 import net.minecraft.launchwrapper.LaunchClassLoader
+import net.minecraftforge.fml.relauncher.CoreModManager
 import org.spongepowered.asm.launch.MixinBootstrap
 import org.spongepowered.asm.mixin.MixinEnvironment
 import org.spongepowered.asm.mixin.Mixins
 import java.io.File
 import java.net.MalformedURLException
+import java.net.URISyntaxException
 
 class CTJSTweaker : ITweaker {
     override fun acceptOptions(args: MutableList<String>?, gameDir: File?, assetsDir: File?, profile: String?) {}
@@ -17,6 +19,21 @@ class CTJSTweaker : ITweaker {
         Mixins.addConfiguration("mixins.ctjs.json")
         MixinEnvironment.getDefaultEnvironment().obfuscationContext = "searge"
         MixinEnvironment.getDefaultEnvironment().side = MixinEnvironment.Side.CLIENT
+
+        // fixing mixins. code from https://github.com/ReplayMod/ReplayMod/blob/0a26f59c9c5ac6ec72b8bdb758ef126f2d10e28d/src/main/java/com/replaymod/core/LoadingPlugin.java#L26
+        val codeSource = javaClass.protectionDomain.codeSource
+        if (codeSource != null) {
+            val location = codeSource.location
+            try {
+                val file = File(location.toURI())
+                if (file.isFile) CoreModManager.getIgnoredMods().remove(file.name)
+            } catch (exception: URISyntaxException) {
+                exception.printStackTrace()
+            }
+        } else {
+            println("No CodeSource, if this is not a development environment we might run into problems!")
+            println(javaClass.protectionDomain)
+        }
 
         try {
             classLoader.addURL(File(System.getProperty("java.home"), "lib/ext/nashorn.jar").toURI().toURL())
