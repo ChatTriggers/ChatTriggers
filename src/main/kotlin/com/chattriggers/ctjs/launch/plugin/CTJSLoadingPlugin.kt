9911a717cad7a478c659d3d94a520a4c4306f00d
mattco98 <matthewcolsson@gmail.com>
2019-07-17 08:41:45 -0500

    Fix asm helper

--- src/main/kotlin/com/chattriggers/ctjs/launch/plugin/CTJSLoadingPlugin.kt
+++ src/main/kotlin/com/chattriggers/ctjs/launch/plugin/CTJSLoadingPlugin.kt
@@ -3,16 +3,16 @@ package com.chattriggers.ctjs.launch.plugin
 import net.minecraftforge.fml.relauncher.IFMLLoadingPlugin
 
 class CTJSLoadingPlugin : IFMLLoadingPlugin {
-    override fun getModContainerClass() = ""
+    override fun getModContainerClass() = null
 
     override fun getASMTransformerClass(): Array<String> {
         println("get transformer class")
         return arrayOf("com.chattriggers.ctjs.launch.plugin.CTJSTransformer")
     }
 
-    override fun getSetupClass() = ""
+    override fun getSetupClass() = null
 
     override fun injectData(data: MutableMap<String, Any>?) {}
 
-    override fun getAccessTransformerClass() = ""
+    override fun getAccessTransformerClass() = null
 }
