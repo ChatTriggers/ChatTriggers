578f8ba12f92b08e8e950a39c9d360469ae44dc0
Andrew Koerber <kerbybit@gmail.com>
2019-07-22 13:58:16 -0500

    Added toString to Message and TextComponent

--- src/main/kotlin/com/chattriggers/ctjs/minecraft/objects/message/Message.kt
+++ src/main/kotlin/com/chattriggers/ctjs/minecraft/objects/message/Message.kt
@@ -192,6 +192,14 @@ class Message {
         )
     }
 
+    override fun toString() =
+            "Message{" +
+                    "formatted=$this.formatted, " +
+                    "recursive=$recursive, " +
+                    "chatLineId=$chatLineId, " +
+                    "messageParts=$messageParts" +
+                    "}"
+
     private fun parseMessage() {
         this.chatMessage = BaseTextComponent("")
 
