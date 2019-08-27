dc4a839a59d38bb199a4e415e7cabce2231d5a4f
FalseHonesty <thefalsehonesty@gmail.com>
2019-08-27 14:56:00 -0400

    Wrap collections to JS arrays

--- src/main/kotlin/com/chattriggers/ctjs/engine/langs/js/JSContextFactory.kt
+++ src/main/kotlin/com/chattriggers/ctjs/engine/langs/js/JSContextFactory.kt
@@ -4,6 +4,7 @@ import net.minecraft.client.Minecraft
 import org.mozilla.javascript.Context
 import org.mozilla.javascript.Context.FEATURE_LOCATION_INFORMATION_IN_ERROR
 import org.mozilla.javascript.ContextFactory
+import org.mozilla.javascript.Scriptable
 import org.mozilla.javascript.WrapFactory
 import org.mozilla.javascript.tools.ToolErrorReporter
 import java.net.URL
@@ -22,7 +23,15 @@ object JSContextFactory : ContextFactory() {
         cx.optimizationLevel = if (optimize) 9 else 0
         cx.languageVersion = Context.VERSION_ES6
         cx.errorReporter = ToolErrorReporter(true, JSLoader.console.out)
-        cx.wrapFactory = WrapFactory().apply { isJavaPrimitiveWrap = false }
+        cx.wrapFactory = object : WrapFactory() {
+            override fun wrap(cx: Context?, scope: Scriptable?, obj: Any?, staticType: Class<*>?): Any? {
+                if (obj is Collection<*>) {
+                    return super.wrap(cx, scope, obj.toTypedArray(), staticType)
+                }
+
+                return super.wrap(cx, scope, obj, staticType)
+            }
+        }.apply { isJavaPrimitiveWrap = false }
     }
 
     override fun hasFeature(cx: Context?, featureIndex: Int): Boolean {
