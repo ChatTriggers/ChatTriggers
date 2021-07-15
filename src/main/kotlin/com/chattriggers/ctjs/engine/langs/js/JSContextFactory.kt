fe9904d6069b08d6b7e85040a92d428e0e30e682
Matthew Olsson <matthewcolsson@gmail.com>
2021-07-15 15:22:36 -0700

    JSContextFactory: Use custom ErrorReporter
    For some reason the ToolErrorReporter can't find it's resource bundle
    correctly. It's much easier to just write a quick custom reporter
    than to actually figure out where that resource is (and then how to
    access it), so that's what I did

--- src/main/kotlin/com/chattriggers/ctjs/engine/langs/js/JSContextFactory.kt
+++ src/main/kotlin/com/chattriggers/ctjs/engine/langs/js/JSContextFactory.kt
@@ -25,7 +25,7 @@ object JSContextFactory : ContextFactory() {
         cx.applicationClassLoader = classLoader
         cx.optimizationLevel = if (optimize) 9 else 0
         cx.languageVersion = Context.VERSION_ES6
-        cx.errorReporter = ToolErrorReporter(true, JSLoader.console.out)
+        cx.errorReporter = JSErrorReporter(JSLoader.console.out)
         cx.wrapFactory = object : WrapFactory() {
             override fun wrap(cx: Context?, scope: Scriptable?, obj: Any?, staticType: Class<*>?): Any? {
                 if (obj is Collection<*>) {
---
+++ src/main/kotlin/com/chattriggers/ctjs/engine/langs/js/JSErrorReporter.kt
@@ -0,0 +1,58 @@
+package com.chattriggers.ctjs.engine.langs.js
+
+import org.mozilla.javascript.ErrorReporter
+import org.mozilla.javascript.EvaluatorException
+import java.io.PrintStream
+
+class JSErrorReporter(private val outputStream: PrintStream) : ErrorReporter {
+    override fun warning(message: String?, sourceName: String?, line: Int, lineSource: String?, lineOffset: Int) {
+        reportErrorMessage(message, sourceName, line, lineSource, lineOffset, isWarning = true)
+    }
+
+    override fun error(message: String?, sourceName: String?, line: Int, lineSource: String?, lineOffset: Int) {
+        reportErrorMessage(message, sourceName, line, lineSource, lineOffset, isWarning = false)
+    }
+
+    private fun reportErrorMessage(
+        inputMessage: String?,
+        sourceName: String?,
+        line: Int,
+        lineSource: String?,
+        lineOffset: Int,
+        isWarning: Boolean,
+    ) {
+        var message = if (line > 0) {
+            if (sourceName == null) {
+                "line $line: $inputMessage"
+            } else "\"$sourceName\", line $line: $inputMessage"
+        } else inputMessage
+
+        if (isWarning)
+            message = "warning: $message"
+
+        outputStream.println(MESSAGE_PREFIX + message)
+        if (lineSource != null) {
+            outputStream.println(MESSAGE_PREFIX + lineSource)
+            outputStream.println(MESSAGE_PREFIX + buildIndicator(lineOffset))
+        }
+    }
+
+    private fun buildIndicator(offset: Int) = buildString {
+        repeat(offset) { append('.') }
+        append('^')
+    }
+
+    override fun runtimeError(
+        message: String?,
+        sourceName: String?,
+        line: Int,
+        lineSource: String?,
+        lineOffset: Int
+    ): EvaluatorException {
+        return EvaluatorException(message, sourceName, line, lineSource, lineOffset)
+    }
+
+    companion object {
+        const val MESSAGE_PREFIX = "js: "
+    }
+}
