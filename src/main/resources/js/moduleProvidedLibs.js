73d48946add823f916ea462e06b7dd44a6cdbce1
Matthew Olsson <matthewcolsson@gmail.com>
2022-01-08 08:59:28 -0700

    Remove GuiHandler
    This removes GuiHandler in favor of Essential's GuiUtil. This is not a
    breaking change.

--- src/main/resources/js/moduleProvidedLibs.js
+++ src/main/resources/js/moduleProvidedLibs.js
@@ -29,7 +29,8 @@ global.DisplayLine = Java.type("com.chattriggers.ctjs.engine.langs.js.JSDisplayL
 global.DisplayHandler = Java.type("com.chattriggers.ctjs.minecraft.objects.display.DisplayHandler");
 
 global.Gui = Java.type("com.chattriggers.ctjs.engine.langs.js.JSGui");
-global.GuiHandler = Java.type("com.chattriggers.ctjs.minecraft.objects.gui.GuiHandler").INSTANCE;
+global.GuiUtil = Java.type("gg.essential.api.utils.GuiUtil");
+global.GuiHandler = { openGui: GuiUtil.open };
 
 global.Message = Java.type("com.chattriggers.ctjs.minecraft.objects.message.Message");
 global.TextComponent = Java.type("com.chattriggers.ctjs.minecraft.objects.message.TextComponent");
