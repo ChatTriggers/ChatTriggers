9aab6cb44976789271a2127419a6e4614f692019
Matthew Olsson <matthewcolsson@gmail.com>
2022-01-14 11:14:11 -0700

    API: Track hashed UUIDs
    These are salted with the user's PC name, which is of course unknown to
    the server and has a lot of variation. It will not be possible to
    derive any meaningful information beyond unique user counts, even if
    given direct access to the server.

--- src/main/kotlin/com/chattriggers/ctjs/CTJS.kt
+++ src/main/kotlin/com/chattriggers/ctjs/CTJS.kt
@@ -10,6 +10,7 @@ import com.chattriggers.ctjs.minecraft.listeners.WorldListener
 import com.chattriggers.ctjs.minecraft.objects.Sound
 import com.chattriggers.ctjs.minecraft.objects.gui.GuiHandler
 import com.chattriggers.ctjs.minecraft.wrappers.CPS
+import com.chattriggers.ctjs.minecraft.wrappers.Player
 import com.chattriggers.ctjs.triggers.TriggerType
 import com.chattriggers.ctjs.utils.Config
 import com.chattriggers.ctjs.utils.UpdateChecker
@@ -21,6 +22,10 @@ import net.minecraftforge.fml.common.Mod
 import net.minecraftforge.fml.common.event.FMLInitializationEvent
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent
 import java.io.File
+import java.net.URL
+import java.security.MessageDigest
+import java.util.*
+import kotlin.concurrent.thread
 
 @Mod(
     modid = Reference.MODID,
@@ -58,8 +63,22 @@ object CTJS {
         Vigilance.initialize()
         Config.preload()
 
-        Reference.conditionalThread {
+        // Ensure that reportHashedUUID always runs on a separate thread
+        if (Config.threadedLoading) {
+            thread {
+                try {
+                    ModuleManager.entryPass()
+                    reportHashedUUID()
+                } catch (e: Exception) {
+                    e.printTraceToConsole()
+                    e.printStackTrace()
+                }
+            }
+        } else {
             ModuleManager.entryPass()
+            thread {
+                reportHashedUUID()
+            }
         }
 
         registerHooks()
@@ -67,7 +86,23 @@ object CTJS {
 
     private fun registerHooks() {
         ClientCommandHandler.instance.registerCommand(CTCommand)
-
         Runtime.getRuntime().addShutdownHook(Thread(TriggerType.GameUnload::triggerAll))
+    }
+
+    private fun reportHashedUUID() {
+        val uuid = Player.getUUID().encodeToByteArray()
+        val salt = (System.getProperty("user.name") ?: "").encodeToByteArray()
+        val md = MessageDigest.getInstance("SHA-256")
+        md.update(salt)
+        val hashedUUID = md.digest(uuid)
+        val hash = Base64.getUrlEncoder().encodeToString(hashedUUID)
+
+        val url = "${WEBSITE_ROOT}/api/statistics/track?hash=$hash"
+        val connection = URL(url).openConnection().apply {
+            setRequestProperty("User-Agent", "Mozilla/5.0")
+            connectTimeout = 5000
+            readTimeout = 5000
+        }
+        connection.getInputStream()
     }
 }
