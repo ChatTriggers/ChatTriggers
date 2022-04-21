0e1d2e0b4bb7d5b80cd9cc6c91469e9afe6cdcde
Cam Walter <cam.n.walter@gmail.com>
2022-04-20 22:01:00 -0500

    KeyBind: Re-add clearing & removing keybinds, Make Client.getKeyBindFrom methods attempt to return an existing KeyBind before making a new one

--- src/main/kotlin/com/chattriggers/ctjs/minecraft/objects/keybind/KeyBindHandler.kt
+++ src/main/kotlin/com/chattriggers/ctjs/minecraft/objects/keybind/KeyBindHandler.kt
@@ -4,25 +4,39 @@ import com.chattriggers.ctjs.minecraft.wrappers.World
 import net.minecraftforge.common.MinecraftForge
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
 import net.minecraftforge.fml.common.gameevent.TickEvent
+import java.util.concurrent.CopyOnWriteArrayList
 
 internal object KeyBindHandler {
     init {
         MinecraftForge.EVENT_BUS.register(this)
     }
 
-    private val keyBinds = mutableListOf<KeyBind>()
+    private val keyBinds = CopyOnWriteArrayList<KeyBind>()
 
     fun registerKeyBind(keyBind: KeyBind) {
         keyBinds.add(keyBind)
     }
 
+    fun unregisterKeyBind(keyBind: KeyBind) {
+        keyBinds.remove(keyBind)
+    }
+
     fun clearKeyBinds() = keyBinds.clear()
+
+    fun getKeyBinds() = keyBinds
 
     @SubscribeEvent
     fun onTick(event: TickEvent.ClientTickEvent) {
-        if (World.getWorld() == null || event.phase == TickEvent.Phase.END)
+        if (!World.isLoaded() || event.phase == TickEvent.Phase.END)
             return
 
-        keyBinds.forEach(KeyBind::onTick)
+        keyBinds.forEach {
+            // TODO: This causes null pointers rarely, crashing the game.
+            //  Catching solves this for now.
+            try {
+                it.onTick()
+            } catch (ignored: Exception) {
+            }
+        }
     }
 }
