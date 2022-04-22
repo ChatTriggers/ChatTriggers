893991e0f3f2da5a313328f96976f92fdeb2a03a
Cam Walter <cam.n.walter@gmail.com>
2022-04-21 19:01:58 -0500

    Slot: Correctly number the slot indices inside GuiContainerCreative

--- src/main/kotlin/com/chattriggers/ctjs/minecraft/wrappers/inventory/Slot.kt
+++ src/main/kotlin/com/chattriggers/ctjs/minecraft/wrappers/inventory/Slot.kt
@@ -1,9 +1,14 @@
 package com.chattriggers.ctjs.minecraft.wrappers.inventory
 
 import com.chattriggers.ctjs.utils.kotlin.MCSlot
+import net.minecraft.client.gui.inventory.GuiContainerCreative.CreativeSlot
 
 class Slot(val mcSlot: MCSlot) {
-    fun getIndex(): Int = mcSlot.slotNumber
+    fun getIndex(): Int = if (mcSlot is CreativeSlot) {
+        mcSlot.slotIndex
+    } else {
+        mcSlot.slotNumber
+    }
 
     fun getDisplayX(): Int = mcSlot.xDisplayPosition
 
@@ -11,7 +16,7 @@ class Slot(val mcSlot: MCSlot) {
 
     fun getInventory(): Inventory = Inventory(mcSlot.inventory)
 
-    fun getItem(): Item? = if (mcSlot.stack != null) Item(mcSlot.stack) else null
+    fun getItem(): Item? = mcSlot.stack?.let(::Item)
 
     override fun toString(): String = "Slot ${getIndex()} of (${getInventory().getClassName()}: ${getInventory().getName()}): ${getItem()}"
 }
