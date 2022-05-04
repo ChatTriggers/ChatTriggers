9431c7b586cf0a39447b355b584a83111ca14456
Matthew Olsson <matthewcolsson@gmail.com>
2022-05-03 23:43:21 -0700

    Triggers: Rename OnRenderTrigger to OnEventTrigger
    This trigger shouldn't need to be limited to only render triggers,
    so let's give it a more generic name and support all event-based
    trigger types.

---
+++ src/main/kotlin/com/chattriggers/ctjs/triggers/OnEventTrigger.kt
@@ -0,0 +1,31 @@
+package com.chattriggers.ctjs.triggers
+
+import com.chattriggers.ctjs.engine.ILoader
+import com.chattriggers.ctjs.minecraft.listeners.CancellableEvent
+import net.minecraftforge.fml.common.eventhandler.Event
+
+class OnEventTrigger(method: Any, triggerType: TriggerType, loader: ILoader) : OnTrigger(method, triggerType, loader) {
+    private var triggerIfCanceled = true
+
+    /**
+     * Sets if this trigger should run if the event has already been canceled.
+     * True by default.
+     *
+     * @param bool Boolean to set
+     * @return the trigger object for method chaining
+     */
+    fun triggerIfCanceled(bool: Boolean) = apply { triggerIfCanceled = bool }
+
+    override fun trigger(args: Array<out Any?>) {
+        val isCanceled = when (val event = args.lastOrNull()) {
+            is CancellableEvent -> event.isCanceled()
+            is Event -> event.isCanceled
+            else -> throw IllegalArgumentException(
+                "Expected last argument of ${type.name} trigger to be an Event, got ${event?.javaClass?.name ?: "null"}"
+            )
+        }
+
+        if (triggerIfCanceled || !isCanceled)
+            callMethod(args)
+    }
+}
