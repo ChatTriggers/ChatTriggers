8dc2e29db61a2136137343155d677f585c3916f6
Matthew Olsson <matthewcolsson@gmail.com>
2022-05-10 22:34:51 -0700

    Build: Update version to 2.1.1

--- src/main/kotlin/com/chattriggers/ctjs/Reference.kt
+++ src/main/kotlin/com/chattriggers/ctjs/Reference.kt
@@ -21,7 +21,7 @@ import kotlin.math.roundToInt
 object Reference {
     const val MODID = "chattriggers"
     const val MODNAME = "ChatTriggers"
-    const val MODVERSION = "2.1.0"
+    const val MODVERSION = "2.1.1"
 
     var isLoaded = true
 
