a75ab0fb0ab9965ce7719741dc2c394a01799b4a
Cam Walter <cam.n.walter@gmail.com>
2022-06-11 19:43:19 -0500

    PlayerMP: Update for Fabric

---
+++ src/main/java/com/chattriggers/ctjs/launch/mixins/transformers/GameProfileAccessor.java
@@ -0,0 +1,17 @@
+package com.chattriggers.ctjs.launch.mixins.transformers;
+
+//#if MC>=11701
+//$$ import com.mojang.authlib.GameProfile;
+//$$ import org.spongepowered.asm.mixin.Final;
+//$$ import org.spongepowered.asm.mixin.Mixin;
+//$$ import org.spongepowered.asm.mixin.Mutable;
+//$$ import org.spongepowered.asm.mixin.gen.Accessor;
+//$$
+//$$ @Mixin(GameProfile.class)
+//$$ public interface GameProfileAccessor {
+//$$     @Final
+//$$     @Mutable
+//$$     @Accessor(remap = false)
+//$$     void setName(String name);
+//$$ }
+//#endif
