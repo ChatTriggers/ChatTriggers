c7d4ac4290553b4b54682e669b2324b3d08de6b6
Cam Walter <cam.n.walter@gmail.com>
2022-06-11 19:04:15 -0500

    CTCommand: Convert to use Essential's Command API

--- src/main/kotlin/com/chattriggers/ctjs/commands/CTCommand.kt
+++ src/main/kotlin/com/chattriggers/ctjs/commands/CTCommand.kt
@@ -5,112 +5,83 @@ import com.chattriggers.ctjs.engine.module.ModuleManager
 import com.chattriggers.ctjs.engine.module.ModulesGui
 import com.chattriggers.ctjs.minecraft.libs.ChatLib
 import com.chattriggers.ctjs.minecraft.listeners.ClientListener
-import com.chattriggers.ctjs.minecraft.objects.gui.GuiHandler
 import com.chattriggers.ctjs.printTraceToConsole
 import com.chattriggers.ctjs.utils.Config
-import com.chattriggers.ctjs.utils.kotlin.MCClickEventAction
 import com.chattriggers.ctjs.utils.kotlin.setChatLineId
 import com.chattriggers.ctjs.utils.kotlin.toVersion
+import gg.essential.api.commands.*
 import gg.essential.api.utils.GuiUtil
 import gg.essential.universal.wrappers.message.UMessage
 import gg.essential.universal.wrappers.message.UTextComponent
+import net.minecraft.event.ClickEvent
 import java.awt.Desktop
 import java.awt.Toolkit
 import java.awt.datatransfer.StringSelection
 import java.io.File
 import java.io.IOException
-import java.util.*
+import gg.essential.api.commands.Command as EssentialCommand
 
-//#if MC==10809
-import net.minecraft.command.CommandBase
-import net.minecraft.command.CommandException
-import net.minecraft.command.ICommandSender
-//#else
-//$$ import com.mojang.brigadier.CommandDispatcher
-//$$ import com.mojang.brigadier.arguments.StringArgumentType
-//$$ import com.mojang.brigadier.builder.LiteralArgumentBuilder
-//$$ import com.mojang.brigadier.context.CommandContext
-//$$ import net.minecraft.commands.CommandSourceStack
-//$$ import net.minecraft.commands.Commands
-//#endif
+object CTCommand : EssentialCommand("ct") {
+    override val commandAliases = setOf(Alias("chattriggers"))
 
-//#if MC==10809
-object CTCommand : CommandBase() {
-//#else
-//$$ object CTCommand {
-//#endif
     private const val idFixed = 90123 // ID for dumped chat
     private var idFixedOffset = -1 // ID offset (increments)
 
-    private val commands = listOf(
-        subcommand("load", "reload") { Reference.loadCT() },
-        subcommand("unload") { Reference.unloadCT() },
-        subcommand("files", "file") { openFileLocation() },
-        subcommand("import") {
-            if (it.isEmpty()) {
-                ChatLib.chat("&c/ct import [module name]")
-            } else import(it[0])
-        },
-        subcommand("delete") {
-            when {
-                it.isEmpty() -> ChatLib.chat("&c/ct delete [module name]")
-                ModuleManager.deleteModule(it[0]) -> ChatLib.chat("&aDeleted")
-                else -> ChatLib.chat("&cFailed to delete ${it[0]}")
+    private fun dumpChat(lines: Int) = dumpList(ClientListener.chatHistory, lines)
+    private fun dumpActionBar(lines: Int) = dumpList(ClientListener.actionBarHistory, lines)
+    private fun dumpList(messages: List<String>, lines: Int) {
+        clearOldDump()
+
+        val toDump = lines.coerceAtMost(messages.size)
+        UMessage("&6&m${ChatLib.getChatBreak()}").setChatLineId(idFixed).chat()
+        var msg: String
+        for (i in 0 until toDump) {
+            msg = ChatLib.replaceFormatting(messages[messages.size - toDump + i])
+            UMessage(
+                UTextComponent(msg).apply {
+                    setClick(ClickEvent.Action.RUN_COMMAND, "/ct copy $msg")
+                    hoverValue = ChatLib.addColor("&eClick here to copy this message.")
+                    formatted = false
+                }
+            ).apply {
+                isFormatted = false
+                chatLineId = idFixed + lines + 1
+                chat()
             }
-        },
-        subcommand("modules") { GuiUtil.open(ModulesGui) },
-        subcommand("console") {
-            if (it.isEmpty()) {
-                ModuleManager.generalConsole.showConsole()
-            } else ModuleManager.getConsole(it[0]).showConsole()
-        },
-        subcommand("config", "settings", "setting") { GuiUtil.open(Config.gui()!!) },
-        subcommand("sim", "simulate") { ChatLib.simulateChat(it.joinToString(" ")) },
-        subcommand("dump") { dump(it) },
-        subcommand("copy") { copyArgsToClipboard(it) },
-    )
-
-    //#if MC==10809
-    override fun getCommandUsage(sender: ICommandSender?) = getUsage()
-
-    override fun getCommandName() = "chattriggers"
-
-    override fun getCommandAliases() = mutableListOf("ct")
-
-    override fun getRequiredPermissionLevel() = 0
-
-    @Throws(CommandException::class)
-    override fun processCommand(sender: ICommandSender?, args: Array<String>) {
-        if (args.isEmpty()) {
-            ChatLib.chat(getUsage())
-            return
         }
+        UMessage("&6&m${ChatLib.getChatBreak()}").setChatLineId(idFixed + lines + 1).chat()
 
-        commands.firstOrNull { args[0] in it.first }?.second?.invoke(args.drop(1))
-            ?: ChatLib.chat(getUsage())
+        idFixedOffset = idFixed + lines + 1
     }
-    //#else
-    //$$ fun register(dispatcher: CommandDispatcher<CommandSourceStack?>) {
-    //$$     var command = Commands.literal("ct")
-    //$$
-    //$$     for ((aliases, action) in commands) {
-    //$$         for (alias in aliases) {
-    //$$             command = command.then(Commands.literal(alias).executes { ctx ->
-    //$$                 action(ctx.nodes.map {
-    //$$                     ctx.input.substring(it.range.start, it.range.end)
-    //$$                 })
-    //$$                 1
-    //$$             })
-    //$$         }
-    //$$     }
-    //$$
-    //$$     dispatcher.register(command)
-    //$$ }
-    //#endif
 
-    private fun subcommand(vararg aliases: String, executor: (List<String>) -> Unit) = Pair(aliases.toList(), executor)
+    private fun clearOldDump() {
+        if (idFixedOffset == -1) return
+        while (idFixedOffset >= idFixed)
+            ChatLib.clearChat(idFixedOffset--)
+        idFixedOffset = -1
+    }
 
-    private fun import(moduleName: String) {
+    @DefaultHandler
+    fun handle() = ChatLib.say("/ct help")
+
+    @SubCommand("load", aliases = ["reload"], description = "Reloads all ChatTriggers modules.")
+    fun load() = Reference.loadCT()
+
+    @SubCommand("unload", description = "Unloads all of ChatTriggers.")
+    fun unload() = Reference.unloadCT()
+
+    @SubCommand("files", aliases = ["file"], description = "Opens the ChatTriggers folder.")
+    fun files() {
+        try {
+            Desktop.getDesktop().open(File("./config/ChatTriggers"))
+        } catch (exception: IOException) {
+            exception.printTraceToConsole()
+            ChatLib.chat("&cCould not open file location")
+        }
+    }
+
+    @SubCommand("import", description = "Imports a module.")
+    fun import(@DisplayName("module name") moduleName: String) {
         if (ModuleManager.cachedModules.any { it.name.equals(moduleName, ignoreCase = true) }) {
             ChatLib.chat("&cModule $moduleName is already installed!")
         } else {
@@ -138,85 +109,40 @@ object CTCommand : CommandBase() {
         }
     }
 
-    private fun getUsage() = """
-        &b&m${ChatLib.getChatBreak()}
-        &c/ct load &7- &oReloads all of the ChatTriggers modules.
-        &c/ct import [module] &7- &oImports a module.
-        &c/ct delete [module] &7- &oDeletes a module.
-        &c/ct files &7- &oOpens the ChatTriggers folder.
-        &c/ct modules &7- &oOpens the modules GUI.
-        &c/ct console [language] &7- &oOpens the ChatTriggers console.
-        &c/ct simulate [message] &7- &oSimulates a received chat message.
-        &c/ct dump &7- &oDumps previous chat messages into chat.
-        &c/ct settings &7- &oOpens the ChatTriggers settings.
-        &c/ct &7- &oDisplays this help dialog.
-        &b&m${ChatLib.getChatBreak()}
-    """.trimIndent()
+    @SubCommand("delete", description = "Deletes a module.")
+    fun delete(@DisplayName("module name") moduleName: String) = when {
+        ModuleManager.deleteModule(moduleName) -> ChatLib.chat("&aDeleted")
+        else -> ChatLib.chat("&cFailed to delete $moduleName")
+    }
 
-    private fun openFileLocation() {
-        try {
-            Desktop.getDesktop().open(File("./config/ChatTriggers"))
-        } catch (exception: IOException) {
-            exception.printTraceToConsole()
-            ChatLib.chat("&cCould not open file location")
+    @SubCommand("modules", description = "Opens the modules GUI.")
+    fun modules() = GuiUtil.open(ModulesGui)
+
+    @SubCommand("console", description = "Opens the ChatTriggers console.")
+    fun console(@DisplayName("console language") language: String?) {
+        if (language == null) {
+            ModuleManager.generalConsole.showConsole()
+        } else ModuleManager.getConsole(language).showConsole()
+    }
+
+    @SubCommand("config", aliases = ["settings", "setting"], description = "Opens the ChatTriggers settings.")
+    fun config() = GuiUtil.open(Config.gui())
+
+    @SubCommand("simulate", aliases = ["sim"], description = "Simulates a received chat message.")
+    fun simulate(@Greedy message: String) = ChatLib.simulateChat(message)
+
+    @SubCommand("dump", description = "Dumps previous chat messages into chat.")
+    fun dump(@Options(["chat", "actionbar"]) type: String?, lines: Int?) {
+        when (type?.lowercase()) {
+            "chat" -> dumpChat(lines ?: 100)
+            "actionbar" -> dumpActionBar(lines ?: 100)
+            null -> dumpChat(100)
         }
     }
 
-    private fun dump(args: List<String>) {
-        if (args.size == 1) {
-            dumpChat()
-            return
-        }
-        when (args[1].lowercase()) {
-            "chat" -> {
-                if (args.size == 2) dumpChat()
-                else dumpChat(args[2].toInt())
-            }
-            "actionbar" -> {
-                if (args.size == 2) dumpActionBar()
-                else dumpActionBar(args[2].toInt())
-            }
-        }
-    }
-
-    private fun dumpChat(lines: Int = 100) = dumpList(ClientListener.chatHistory, lines)
-    private fun dumpActionBar(lines: Int = 100) = dumpList(ClientListener.actionBarHistory, lines)
-    private fun dumpList(messages: List<String>, lines: Int) {
+    @SubCommand("copy", description = "Copies a message to the clipboard. Used in /ct dump.")
+    fun copy(@Greedy message: String?) {
         clearOldDump()
-
-        var toDump = lines
-        if (toDump > messages.size) toDump = messages.size
-        UMessage("&6&m${ChatLib.getChatBreak()}").setChatLineId(idFixed).chat()
-        var msg: String
-        for (i in 0 until toDump) {
-            msg = ChatLib.replaceFormatting(messages[messages.size - toDump + i])
-            UMessage(
-                UTextComponent(msg).apply {
-                    setClick(MCClickEventAction.RUN_COMMAND, "/ct copy $msg")
-                    hoverValue = ChatLib.addColor("&eClick here to copy this message.")
-                    formatted = false
-                }
-            ).apply {
-                isFormatted = false
-                chatLineId = idFixed + lines + 1
-                chat()
-            }
-        }
-        UMessage("&6&m${ChatLib.getChatBreak()}").setChatLineId(idFixed + lines + 1).chat()
-
-        idFixedOffset = idFixed + lines + 1
-    }
-
-    private fun clearOldDump() {
-        if (idFixedOffset == -1) return
-        while (idFixedOffset >= idFixed)
-            ChatLib.clearChat(idFixedOffset--)
-        idFixedOffset = -1
-    }
-
-    private fun copyArgsToClipboard(args: List<String>) {
-        clearOldDump()
-        val toCopy = args.drop(1).joinToString(" ")
-        Toolkit.getDefaultToolkit().systemClipboard.setContents(StringSelection(toCopy), null)
+        Toolkit.getDefaultToolkit().systemClipboard.setContents(StringSelection(message), null)
     }
 }
