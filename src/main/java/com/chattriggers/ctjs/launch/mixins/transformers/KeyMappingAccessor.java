32c1cbb8e93b8cb8030303414523bf572be2fd29
Cam Walter <cam.n.walter@gmail.com>
2022-06-11 19:40:30 -0500

    KeyBind: Update for Fabric

--- src/main/java/com/chattriggers/ctjs/launch/mixins/transformers/KeyMappingAccessor.java
+++ src/main/java/com/chattriggers/ctjs/launch/mixins/transformers/KeyMappingAccessor.java
@@ -1,17 +1,19 @@
 package com.chattriggers.ctjs.launch.mixins.transformers;
 
 //#if MC>=11701
+//$$ import com.mojang.blaze3d.platform.InputConstants;
 //$$ import net.minecraft.client.KeyMapping;
-//$$ import net.minecraftforge.client.settings.KeyBindingMap;
 //$$ import org.spongepowered.asm.mixin.Mixin;
 //$$ import org.spongepowered.asm.mixin.gen.Accessor;
 //$$
-//$$ import java.util.Map;
 //$$ import java.util.Set;
 //$$
 //$$ @Mixin(KeyMapping.class)
 //$$ public interface KeyMappingAccessor {
 //$$     @Accessor("CATEGORIES")
 //$$     static Set<String> getCategories() { return null; }
+//$$
+//$$     @Accessor("key")
+//$$     InputConstants.Key getKey();
 //$$ }
 //#endif
