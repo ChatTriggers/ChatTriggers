1dadfca5cf80d34283d356e8169f0ec137c6e55c
Cam Walter <cam.n.walter@gmail.com>
2022-06-18 17:54:52 -0500

    Client: Verify and Convert

---
+++ src/main/java/com/chattriggers/ctjs/launch/mixins/transformers/MinecraftAccessor.java
@@ -0,0 +1,17 @@
+package com.chattriggers.ctjs.launch.mixins.transformers;
+
+import net.minecraft.client.Minecraft;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+@Mixin(Minecraft.class)
+public interface MinecraftAccessor {
+    //#if MC<=11202
+    @Accessor("debugFPS")
+    //#elseif MC>=11701
+    //$$ @Accessor("fps")
+    //#endif
+    static int getFps() {
+        throw new IllegalStateException("Mixin not applied!");
+    }
+}
