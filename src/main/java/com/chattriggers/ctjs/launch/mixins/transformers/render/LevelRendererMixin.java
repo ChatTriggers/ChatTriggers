93f39ebce6035eb8b0493d44fea73db71a70a26f
Cam Walter <cam.n.walter@gmail.com>
2022-07-22 17:36:17 -0500

    Renderer: Convert drawStringInWorld to 1.17
    Also moved RenderWorld trigger to where it injects for Fabric.
    Changed the scaling to be 20x smaller. Before, a scale of 1 was enormous, and now it is very similar to a regular nametag's size.
    Also fixes the issue of having 2 vastly different text sizes depending on whether increase is toggled or not.

--- src/main/java/com/chattriggers/ctjs/launch/mixins/transformers/render/LevelRendererMixin.java
+++ src/main/java/com/chattriggers/ctjs/launch/mixins/transformers/render/LevelRendererMixin.java
@@ -1,12 +1,16 @@
 package com.chattriggers.ctjs.launch.mixins.transformers.render;
 
 //#if MC>=11701
-//$$ import com.chattriggers.ctjs.minecraft.listeners.events.CancellableEvent;
+//$$ import com.chattriggers.ctjs.minecraft.libs.renderer.Renderer;
 //$$ import com.chattriggers.ctjs.triggers.TriggerType;
 //$$ import com.mojang.blaze3d.vertex.PoseStack;
 //$$ import com.mojang.blaze3d.vertex.VertexConsumer;
+//$$ import com.mojang.math.Matrix4f;
 //$$ import com.mojang.math.Vector3f;
+//$$ import net.minecraft.client.Camera;
+//$$ import net.minecraft.client.renderer.GameRenderer;
 //$$ import net.minecraft.client.renderer.LevelRenderer;
+//$$ import net.minecraft.client.renderer.LightTexture;
 //$$ import net.minecraft.core.BlockPos;
 //$$ import net.minecraft.world.entity.Entity;
 //$$ import net.minecraft.world.level.block.state.BlockState;
@@ -26,6 +30,12 @@ package com.chattriggers.ctjs.launch.mixins.transformers.render;
 //$$         Vector3f pos = new Vector3f(arg4.getX(), arg4.getY(), arg4.getZ());
 //$$
 //$$         TriggerType.BlockHighlight.triggerAll(pos, ci);
+//$$     }
+//$$
+//$$     @Inject(method = "renderLevel", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/LevelRenderer;renderDebug(Lnet/minecraft/client/Camera;)V"))
+//$$     private void chattriggers_renderWorldTrigger(PoseStack arg, float g, long m, boolean bl, Camera arg2, GameRenderer arg3, LightTexture arg4, Matrix4f arg5, CallbackInfo ci) {
+//$$         Renderer.setPartialTicks$chattriggers(g);
+//$$         TriggerType.RenderWorld.triggerAll(g);
 //$$     }
 //$$ }
 //#endif
