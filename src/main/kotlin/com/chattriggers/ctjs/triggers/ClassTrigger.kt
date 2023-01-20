153f65eeaa3f215021e6641568ff5eadaa7394b7
Cakend123 <Marvin.sachs@gmx.net>
2023-01-20 19:06:09 +0100

    ClassTrigger: combines RenderEntity and PacketTrigger in one and add support for TileEntities, to be able to set it running only for certain classes

--- src/main/kotlin/com/chattriggers/ctjs/triggers/ClassTrigger.kt
+++ src/main/kotlin/com/chattriggers/ctjs/triggers/ClassTrigger.kt
@@ -10,8 +10,16 @@ import net.minecraft.network.Packet
 class ClassTrigger(method: Any,private val triggerType: TriggerType, loader: ILoader) : Trigger(method, triggerType, loader) {
     private var triggerClasses: List<Class<*>> = emptyList()
 
+    @Deprecated("Use the better named method", ReplaceWith("setClass(MyClass.class)"))
+    fun setPacketClass(clazz: Class<*>?) = setClasses(listOfNotNull(clazz))
+
+    @Deprecated("Use the better named method", ReplaceWith("setClasses([A.class, B.class, C.class])"))
+    fun setPacketClasses(classes: List<Class<*>>) = setClasses(classes)
+
     /**
-     * Alias for `setClasses([clazz])`
+     * Alias for `setClasses([A.class, B.class])`
+     *
+     * @param clazz The class for which this trigger should run for
      */
     fun setClass(clazz: Class<*>?) = setClasses(listOfNotNull(clazz))
 
@@ -32,27 +40,24 @@ class ClassTrigger(method: Any,private val triggerType: TriggerType, loader: ILo
 
     private fun evalTriggerType(args: Array<out Any?>): Any {
          return when(this.triggerType) {
-             TriggerType.PacketReceived -> returnPacket(args.getOrNull(0), TriggerType.PacketReceived)
-             TriggerType.PacketSent -> returnPacket(args.getOrNull(0), TriggerType.PacketSent)
-             TriggerType.RenderEntity -> returnEntity(args.getOrNull(0), TriggerType.RenderEntity)
-             TriggerType.PostRenderEntity -> returnEntity(args.getOrNull(0), TriggerType.PostRenderEntity)
-             TriggerType.RenderTileEntity -> returnTileEntity(args.getOrNull(0), TriggerType.RenderTileEntity)
-             TriggerType.PostRenderTileEntity -> returnTileEntity(args.getOrNull(0), TriggerType.PostRenderTileEntity)
+             TriggerType.PacketReceived, TriggerType.PacketSent -> returnPacket(args.getOrNull(0))
+             TriggerType.RenderEntity, TriggerType.PostRenderEntity -> returnEntity(args.getOrNull(0))
+             TriggerType.RenderTileEntity, TriggerType.PostRenderTileEntity -> returnTileEntity(args.getOrNull(0))
              else -> {
                  error("Logging args: ${this.triggerType}")
              }
         }
     }
 
-    private fun returnEntity(obj: Any?, type: TriggerType): MCEntity {
-        return (obj as? Entity ?: error("Expected first argument of $type trigger to be instance of net.minecraft.entity.Entity")).entity
+    private fun returnEntity(obj: Any?): MCEntity {
+        return (obj as? Entity ?: error("Expected first argument of ${this.triggerType} trigger to be instance of net.minecraft.entity.Entity")).entity
     }
 
-    private fun returnPacket(obj: Any?, type: TriggerType): Packet<*> {
-        return obj as? Packet<*> ?: error("Expected first argument of $type trigger to be instance of Paket<*> class")
+    private fun returnPacket(obj: Any?): Packet<*> {
+        return obj as? Packet<*> ?: error("Expected first argument of ${this.triggerType} trigger to be instance of Paket<*> class")
     }
 
-    private fun returnTileEntity(obj: Any?, type: TriggerType): MCTileEntity {
-        return (obj as? TileEntity ?: error("Expected first argument of $type trigger to be instance of net.minecraft.tileentity.TileEntity")).tileEntity
+    private fun returnTileEntity(obj: Any?): MCTileEntity {
+        return (obj as? TileEntity ?: error("Expected first argument of ${this.triggerType} trigger to be instance of net.minecraft.tileentity.TileEntity")).tileEntity
     }
 }
